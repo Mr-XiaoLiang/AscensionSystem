8569d32f8f0aca86e00fa4dfc0546105152cb70e
lollipop <liujian594@hotmail.com>
2020-03-16 00:20:57 +0800

    开始基础框架的搭建

--- app/src/main/java/com/lollipop/ascensionsystem/activity/MainActivity.kt
+++ app/src/main/java/com/lollipop/ascensionsystem/activity/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.lollipop.ascensionsystem.activity
 
+import android.os.Bundle
+import com.lollipop.ascensionsystem.R
 import com.lollipop.ascensionsystem.base.BaseActivity
 
 class MainActivity : BaseActivity() {
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
+    override val layoutId: Int
+        get() = R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
 }
