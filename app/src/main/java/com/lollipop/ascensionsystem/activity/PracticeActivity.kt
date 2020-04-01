1bd0ed0571657c8c80dad98e0710cec20773498a
lollipop <liujian594@hotmail.com>
2020-04-02 00:29:42 +0800

    开始基础框架的搭建

---
+++ app/src/main/java/com/lollipop/ascensionsystem/activity/PracticeActivity.kt
@@ -0,0 +1,17 @@
+package com.lollipop.ascensionsystem.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lollipop.ascensionsystem.R
+
+/**
+ * 练功的Activity
+ * @author Lollipop
+ */
+class PracticeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice)
+    }
+}
