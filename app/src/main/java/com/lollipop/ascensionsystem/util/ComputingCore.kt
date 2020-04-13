20d47a9121a1317a77e23a9c88edf297bf57310a
lollipop <liujian594@hotmail.com>
2020-04-14 00:07:20 +0800

    开始基础框架的搭建

--- app/src/main/java/com/lollipop/ascensionsystem/util/ComputingCore.kt
+++ app/src/main/java/com/lollipop/ascensionsystem/util/ComputingCore.kt
@@ -1,5 +1,7 @@
 package com.lollipop.ascensionsystem.util
 
+import com.lollipop.ascensionsystem.info.RoleInfo
+
 /**
  * @author lollipop
  * @date 2020/4/13 00:23
@@ -11,7 +13,15 @@ object ComputingCore {
      * 计算资质的token凭证
      */
     fun qualificationToken(): String {
+        // https://blog.csdn.net/gjy211/article/details/52015198
         return ""
+    }
+
+    /**
+     * 初始化信息
+     */
+    fun initRole(info: RoleInfo) {
+        // TODO
     }
 
 }
