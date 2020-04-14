2351e22427462f1f63efb4cdca319b8b2de724ed
lollipop <liujian594@hotmail.com>
2020-04-15 00:08:39 +0800

    开始基础框架的搭建

--- app/src/main/java/com/lollipop/ascensionsystem/util/ComputingCore.kt
+++ app/src/main/java/com/lollipop/ascensionsystem/util/ComputingCore.kt
@@ -1,5 +1,6 @@
 package com.lollipop.ascensionsystem.util
 
+import android.os.Build
 import com.lollipop.ascensionsystem.info.RoleInfo
 
 /**
@@ -9,12 +10,31 @@ import com.lollipop.ascensionsystem.info.RoleInfo
  */
 object ComputingCore {
 
+    private const val RADIX = 36
+
     /**
      * 计算资质的token凭证
      */
     fun qualificationToken(): String {
         // https://blog.csdn.net/gjy211/article/details/52015198
-        return ""
+        val tokenBuilder = StringBuilder()
+        tokenBuilder.append(Build.VERSION.SDK_INT.token())
+        return tokenBuilder.toString()
+    }
+
+    private fun Int.token(): String {
+        val intValue = this.toString(RADIX)
+        return when {
+            intValue.length < 2 -> {
+                "0$intValue"
+            }
+            intValue.length > 2 -> {
+                intValue.substring(intValue.length - 2, intValue.length)
+            }
+            else -> {
+                intValue
+            }
+        }
     }
 
     /**
